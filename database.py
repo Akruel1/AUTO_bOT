f49536f039f88a56717994bfa534413421c6eebe
Akruel1 <tecca5864@gmail.com>
2025-05-18 10:13:54 +0300

    database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-DB_URL = "sqlite+aiosqlite:///autobot.db"
+DB_URL = "postgresql://postgres:RsOrzlycMdoQuxIsCSWrjhvNglCslluY@postgres.railway.internal:5432/railway"
 
 engine = create_async_engine(DB_URL, echo=False)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
