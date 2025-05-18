56cf82dbf4c005dffbc7c5964c22b650616e9c0f
Akruel1 <tecca5864@gmail.com>
2025-05-18 10:59:13 +0300

    database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-DB_URL = os.getenv("DATABASE_PUBLIC_URL")  # берем из переменных окружения
+DB_URL = os.getenv("MYSQL_PUBLIC_URL")  # берем из переменных окружения
 
 engine = create_async_engine(DB_URL, echo=False)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
