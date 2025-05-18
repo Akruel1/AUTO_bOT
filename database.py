e4a5bff0ce1782a42b76b9b1c3c336ecdb6d794e
Akruel1 <tecca5864@gmail.com>
2025-05-18 10:51:33 +0300

    database.py

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-DB_URL = "postgresql+asyncpg://postgres:XZmhDcTQXxZhJhZdIQDMYbFwMNMgAbwM@yamabiko.proxy.rlwy.net:38820/railway"
+DB_URL = os.getenv("DATABASE_PUBLIC_URL")  # берем из переменных окружения
 
 engine = create_async_engine(DB_URL, echo=False)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
@@ -12,3 +13,8 @@ class Base(DeclarativeBase):
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+# Функция для получения сессии
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
