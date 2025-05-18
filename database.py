0353e8f125a81394463aa3df6464a91b356dab08
Akruel1 <tecca5864@gmail.com>
2025-05-18 10:25:27 +0300

    database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-DB_URL = "postgresql://postgres:XZmhDcTQXxZhJhZdIQDMYbFwMNMgAbwM@yamabiko.proxy.rlwy.net:38820/railway"
+DB_URL = "postgresql+asyncpg://postgres:XZmhDcTQXxZhJhZdIQDMYbFwMNMgAbwM@yamabiko.proxy.rlwy.net:38820/railway"
 
 engine = create_async_engine(DB_URL, echo=False)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
