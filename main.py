3343b7f79e8e62b2c70173a5e2650b6d93fa6b36
Akruel1 <tecca5864@gmail.com>
2025-08-20 09:14:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-import asyncio
+import asyncio 
 import logging
+
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
+
 from config import BOT_TOKEN
 from database import init_db
+
+# user handlers
 from handlers.user import start, profile, purchase, topup, support
-from handlers.admin import broadcast, set_wallet, support_panel, manage_products, topup_admin, delete_all, Admin_text, \
-    change_text
+
+# admin handlers
+from handlers.admin import (
+    broadcast,
+    set_wallet,
+    support_panel,
+    manage_products,
+    topup_admin,
+    delete_all,
+    admin_announce_discount,
+    Admin_text,
+    change_text,
+    product_summary,     # сводка по товарам
+    delete_products      # удаление товаров без фото
+)
+
 from utils.topup_checker import check_topups
 
 # Настройка логгера
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 async def main():
     # Инициализация базы данных
@@ -41,10 +60,13 @@ async def main():
         delete_all.router,
         Admin_text.router,
         change_text.router,
+        admin_announce_discount.router,
+        product_summary.router,   # сводка по товарам
+        delete_products.router    # удаление товаров без фото
     )
-    from keyboards.user_kb import router as user_kb_router
 
-   
+    # Регистрируем пользовательскую клавиатуру
+    from keyboards.user_kb import router as user_kb_router
     dp.include_router(user_kb_router)
 
     # Запуск проверки пополнений
@@ -53,5 +75,6 @@ async def main():
     logger.info("Bot is running...")
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
+
+if __name__ == "__main__": 
     asyncio.run(main())
